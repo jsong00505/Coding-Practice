8c959e7849aa4f2f1de5da78a604a5256336f517
jsong00505 <jsong00505@gmail.com>
2017-03-31 15:15:32 +0900

    chore: hihi

---
+++ code-ground/src/normal2/Solution.java
@@ -0,0 +1,17 @@
+package normal2;
+
+/**
+ * Created by jsong on 31/03/2017.
+ * <p>
+ * hackerrank	: https://www.hackerrank.com/jsong00505
+ * github		: https://github.com/jsong00505
+ * linkedin		: https://www.linkedin.com/in/junesongskorea/
+ * email		: jsong00505@gmail.com
+ * <p>
+ * challenge	: New Room
+ */
+public class Solution {
+	public static void main(String[] args) {
+
+	}
+}
