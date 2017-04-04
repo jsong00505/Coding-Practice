45b0966d894bf48ba84e650c0ffb7eb188024b8d
jsong00505 <jsong00505@gmail.com>
2017-04-04 18:28:38 +0900

    feat: factorial, fibonacci, stack

---
+++ programming-contest-challenging/src/chapter2/beginner/example1/Solution.java
@@ -0,0 +1,44 @@
+package chapter2.beginner.example1;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Created by jsong on 04/04/2017.
+ * <p>
+ * hackerrank	: https://www.hackerrank.com/jsong00505
+ * github		: https://github.com/jsong00505
+ * linkedin		: https://www.linkedin.com/in/junesongskorea/
+ * email		: jsong00505@gmail.com
+ * <p>
+ * challenge	: Factorial
+ */
+public class Solution {
+	static long getFactorialByForLoop(int n) {
+		long result = 1;
+		for(int i = n; i > 0; i--) {
+			result *= i;
+		}
+
+		return result;
+	}
+	static long getFactorialByRecursive(int n) {
+		if(n == 1) {
+			return 1;
+		}
+		return n * getFactorialByRecursive(n - 1);
+	}
+	public static void main(String[] args) {
+		try(
+			Scanner in = new Scanner(System.in);
+			PrintWriter out = new PrintWriter(System.out);
+		) {
+			int n = in.nextInt();
+
+			out.println(getFactorialByRecursive(n));
+			out.println(getFactorialByForLoop(n));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ programming-contest-challenging/src/chapter2/beginner/example3/Solution.java
@@ -0,0 +1,40 @@
+package chapter2.beginner.example3;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Created by jsong on 04/04/2017.
+ * <p>
+ * hackerrank	: https://www.hackerrank.com/jsong00505
+ * github		: https://github.com/jsong00505
+ * linkedin		: https://www.linkedin.com/in/junesongskorea/
+ * email		: jsong00505@gmail.com
+ * <p>
+ * challenge	: Stack
+ */
+public class Solution {
+	public static void main(String[] args) {
+		try(
+			Scanner in = new Scanner(System.in);
+			PrintWriter out = new PrintWriter(System.out);
+		) {
+			Stack<Integer> stack = new Stack<>();	// {}
+			stack.push(1);						// {1}
+			stack.push(2);						// {1, 2}
+			stack.push(3);						// {1, 2, 3}
+			out.println(stack.peek());
+			stack.pop();							// {1, 2}
+			out.println(stack.peek());
+			stack.pop();							// {1}
+			out.println(stack.peek());
+			stack.pop();							// {}
+		} catch(Exception e) {
+
+		}
+
+
+
+	}
+}
