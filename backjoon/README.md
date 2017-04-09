d107bf4ca4f85b3bfe20e9683f4ea3b805d3f076
jsong00505 <jsong00505@gmail.com>
2017-04-09 19:08:10 +0900

    feat: Cow Arts

--- backjoon/README.md
+++ backjoon/README.md
@@ -21,9 +21,11 @@
 |  2178 | [Maze](https://www.acmicpc.net/problem/2178)                                | [Main.java](src/bj217x/bj2178/Main.java?ts=4)   |  9788 KB |  124 MS |        |
 |  2493 | [Tower](https://www.acmicpc.net/problem/2493)                               | [Main.java](src/bj249x/bj2493/Main.java?ts=4)   | 16456 KB | 1928 MS |        |
 |  2504 | [The Price of Brackets](https://www.acmicpc.net/problem/2504)               | [Main.java](src/bj250x/bj2504/Main.java?ts=4)   |  9476 KB |   92 MS |        |
-|  2573 | [Iceberg](https://www.acmicpc.net/problem/2573)                             | [Main.java](src/bj257x/bj2573/Main.java?ts=4)   |          |         |     NS |
+|  2573 | [Iceberg](https://www.acmicpc.net/problem/2573)                             | [Main.java](src/bj257x/bj2573/Main.java?ts=4)   |          |         |     WA |
 |  2667 | [House Number](https://www.acmicpc.net/problem/2667)                        | [Main.java](src/bj266x/bj2667/Main.java?ts=4)   |  8952 KB |  100 MS |        |
 |  2879 | [Pretty Coding](https://www.acmicpc.net/problem/2879)                       | [Main.java](src/bj287x/bj2879/Main.java?ts=4)   | 10576 KB |  176 MS |        |
+|  2974 | [Umnozak](https://www.acmicpc.net/problem/2974)                             | [Main.java](src/bj297x/bj2974/Main.java?ts=4)   |          |         |     NS |
 |  5397 | [Key Logger](https://www.acmicpc.net/problem/5397)                          | [Main.java](src/bj539x/bj5397/Main.java?ts=4)   | 19616 KB | 2992 MS |        |
 |  9935 | [String Explosion](https://www.acmicpc.net/problem/9935)                    | [Main.java](src/bj993x/bj9935/Main.java?ts=4)   | 17180 KB |  560 MS |        |
+| 10026 | [Cow Art](https://www.acmicpc.net/problem/10026)                            | [Main.java](src/bj1002x/bj10026/Main.java?ts=4) | 10496 KB |  132 MS |        |
 | 10799 | [A Steel Rod](https://www.acmicpc.net/problem/10799)                        | [Main.java](src/bj1079x/bj10799/Main.java?ts=4) | 12576 KB |  220 MS |        |
---
+++ backjoon/src/bj1002x/bj10026/Main.java
@@ -0,0 +1,133 @@
+package bj1002x.bj10026;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Created by jsong on 09/04/2017.
+ *
+ * @hackerrank https://www.hackerrank.com/jsong00505
+ * @backjoon https://www.acmicpc.net/user/jsong00505
+ * @github https://github.com/jsong00505
+ * @linkedin https://www.linkedin.com/in/junesongskorea/
+ * @email jsong00505@gmail.com
+ * @challenge Cow Arts
+ */
+public class Main {
+  static char RED = 'R';
+  static char GREEN = 'G';
+  static char BLUE = 'B';
+
+  static int numberOfColoredSection = 0;
+  static int numberOfColoredSectionByBlind = 0;
+
+  /**
+   * debugging method that print out all elements in 2d array
+   *
+   * @param n
+   * @param m
+   * @param map
+   */
+  static void printArray(int n, int m, boolean[][] map) {
+    System.out.println("==============START==============");
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        System.out.print((map[i][j] == true ? "t" : "f") + " ");
+      }
+      System.out.println();
+    }
+
+    System.out.println("==============END==============");
+  }
+
+  static boolean isSameColor(char firstColor, char secondColor, boolean colorBlind) {
+    boolean result = false;
+    if(colorBlind) {
+      if(firstColor != secondColor) {
+        if((firstColor == RED && secondColor == GREEN) || (firstColor == GREEN && secondColor == RED)) {
+          result = true;
+        }
+      } else {
+        result = true;
+      }
+    } else if(firstColor == secondColor) {
+      result = true;
+    }
+
+    return result;
+  }
+  static boolean[][] depthFirstSearch(int n, int x, int y, char[][] map, boolean[][] visited, char color, boolean colorBlind) {
+    visited[x][y] = true;
+
+    // up
+    if(x > 0) {
+      if(isSameColor(map[x][y], map[x - 1][y], colorBlind) && !visited[x - 1][y]) {
+        visited = depthFirstSearch(n, x - 1, y, map, visited, map[x - 1][y], colorBlind);
+      }
+    }
+
+    // down
+    if(x < n - 1) {
+      if(isSameColor(map[x][y], map[x + 1][y], colorBlind) && !visited[x + 1][y]) {
+        visited = depthFirstSearch(n, x + 1, y, map, visited, map[x + 1][y], colorBlind);
+      }
+    }
+    // right
+    if(y < n - 1) {
+      if(isSameColor(map[x][y], map[x][y + 1], colorBlind) && !visited[x][y + 1]) {
+        visited = depthFirstSearch(n, x, y + 1, map, visited, map[x][y + 1], colorBlind);
+      }
+    }
+    // left
+    if(y > 0) {
+      if(isSameColor(map[x][y], map[x][y - 1], colorBlind) && !visited[x][y - 1]) {
+        visited = depthFirstSearch(n, x, y - 1, map, visited, map[x][y - 1], colorBlind);
+      }
+    }
+    return visited;
+  }
+  static void findNumberOfColoredSection(int n, char[][] map) {
+    boolean[][] visited = new boolean[n][n];
+    boolean[][] blindVisited = new boolean[n][n];
+
+
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < n; j++) {
+        if(!visited[i][j]) {
+          visited = depthFirstSearch(n, i, j, map, visited, map[i][j], false);
+          numberOfColoredSection++;
+        }
+
+        if(!blindVisited[i][j]) {
+          blindVisited = depthFirstSearch(n, i, j, map, blindVisited, map[i][j], true);
+          numberOfColoredSectionByBlind++;
+        }
+      }
+    }
+  }
+  public static void main(String[] args) {
+    try (Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out); ) {
+      int n = in.nextInt();
+
+      // validation
+      assert (n >= 1 && n <= 100);
+
+      // declare a n x n array
+      char[][] map = new char[n][n];
+      for (int i = 0; i < n; i++) {
+        String line = in.next();
+        for (int j = 0; j < n; j++) {
+          map[i][j] = line.charAt(j);
+
+          // validation
+          assert(map[i][j] == RED || map[i][j] == GREEN || map[i][j] == BLUE);
+        }
+      } // for
+
+      findNumberOfColoredSection(n, map);
+      out.println(numberOfColoredSection);
+      out.println(numberOfColoredSectionByBlind);
+    }
+  }
+}
