6b0ab8d5d509c0f1bb758c64cf2d85664b9b7f44
jsong00505 <jsong00505@gmail.com>
2017-06-04 13:56:15 -0700

    study: Minimum Absolute Difference in an Array with only skeleton code

---
+++ hackerrank/src/corecs/algorithms/sorting/minimumabsolutediff/Solution.java
@@ -0,0 +1,41 @@
+package corecs.algorithms.sorting.minimumabsolutediff;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Created by jsong on 04/06/2017.
+ *
+ * @hackerrank https://www.hackerrank.com/jsong00505
+ * @backjoon https://www.acmicpc.net/user/jsong00505
+ * @github https://github.com/jsong00505
+ * @linkedin https://www.linkedin.com/in/junesongskorea/
+ * @email jsong00505@gmail.com
+ * @challenge Minimum Absolute Difference in an Array
+ */
+public class Solution {
+  public static void main(String[] args) {
+    try (Scanner in = new Scanner(System.in);
+         PrintWriter out = new PrintWriter(System.out)) {
+      // initialize and declare the number of integers
+      int n = in.nextInt();
+
+      // validation
+      assert (1 < n && n <= 10000);
+
+      // initialize an array of the integers
+      int[] a = new int[n];
+      for (int i = 0; i < n; i++) {
+        // set a integer
+        a[n] = in.nextInt();
+
+        // validation
+        assert (-1000000000 <= n && n <= 1000000000);
+      }
+
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
