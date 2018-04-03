abf92bf8b748294623b541a19b779749403482dc
Jun Song <jsong00505@gmail.com>
2018-04-03 16:06:00 -0700

    refactor: solved with the better solution and beats 71.25%

--- leetcode/src/no014/longest/common/prefix/Solution.java
+++ leetcode/src/no014/longest/common/prefix/Solution.java
@@ -3,33 +3,26 @@ package no014.longest.common.prefix;
 import java.util.Arrays;
 
 public class Solution {
+
   public static String longestCommonPrefix(String[] strs) {
-    if(strs.length < 1) {
+
+    // defensive code
+    if (strs.length < 1) {
       return "";
     }
 
-    Arrays.sort(strs, (o1, o2) -> {
-      if(o1.length() > o2.length()) {
-        return 1;
-      } else if(o1.length() < o2.length()){
-        return -1;
-      } else {
-        return o1.compareTo(o2);
-      }
-    });
+    // sort for optimization. O(nlogn)
+    Arrays.sort(strs);
 
-    StringBuilder result = new StringBuilder(strs[0]);
-    while(!"".equals(result.toString())) {
-      boolean flag = true;
-      for(String str: strs) {
-        if(!str.startsWith(result.toString())) {
-          result.delete(result.length() - 1, result.length());
-          flag = false;
-          break;
-        }
-      }
-      if(flag) {
+    // only compare the first string to last string since the array is sorted. O(length(first))
+    StringBuilder result = new StringBuilder("");
+    String first = strs[0];
+    String last = strs[strs.length - 1];
+    for (int i = 0; i < first.length(); i++) {
+      if (first.charAt(i) != last.charAt(i)) {
         break;
+      } else {
+        result.append(first.charAt(i));
       }
     }
 
@@ -37,10 +30,13 @@ public class Solution {
   }
 
   public static void main(String[] args) {
-    System.out.println(longestCommonPrefix(new String[] {}));
-    System.out.println(longestCommonPrefix(new String[] {""}));
-    System.out.println(longestCommonPrefix(new String[] {"a", "b"}));
-    System.out.println(longestCommonPrefix(new String[] {"a", "bc", "ab"}));
-    System.out.println(longestCommonPrefix(new String[] {"hello", "hel", "hella", "he"}));
+    System.out.println("".equals(longestCommonPrefix(new String[]{})));
+    System.out.println("".equals(longestCommonPrefix(new String[]{""})));
+    System.out.println("".equals(longestCommonPrefix(new String[]{"a", "b"})));
+    System.out.println("".equals(longestCommonPrefix(new String[]{"a", "bc", "ab"})));
+    System.out
+        .println("a".equals(longestCommonPrefix(new String[]{"acd", "abcdef", "acde", "acdef"})));
+    System.out
+        .println("he".equals(longestCommonPrefix(new String[]{"hello", "hel", "hella", "he"})));
   }
 }
