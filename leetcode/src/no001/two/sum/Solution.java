6b52c2ccc086237ea866452e768eec87ca552dea
Jun Song <jsong00505@gmail.com>
2018-04-10 09:06:58 -0700

    feat: solved no1

---
+++ leetcode/src/no001/two/sum/Solution.java
@@ -0,0 +1,18 @@
+package no001.two.sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> numsMap = new HashMap<>();
+    for(int i=0;i<nums.length;i++) {
+      int complement = target - nums[i];
+      if(numsMap.containsKey(complement)) {
+        return new int[] {i, numsMap.get(complement)};
+      }
+      numsMap.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+  }
+}
